2935dda79b6a734703acf1cce284fd19de50a05d
Fabio Del Bene <delbio87@gmail.com>
2016-04-16 00:22:43 +0200

    fueltype and petrolstation formtype and validation, api create new petrolstation

--- src/RestAPIBundle/Controller/PetrolStationController.php
+++ src/RestAPIBundle/Controller/PetrolStationController.php
@@ -2,10 +2,15 @@
 
 namespace RestAPIBundle\Controller;
 
+use DataBundle\Entity\PetrolStation;
+use DataBundle\Form\PetrolStationType;
 use FOS\RestBundle\Controller\Annotations;
 use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\View\View;
 
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+use Symfony\Component\Form\FormTypeInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 class PetrolStationController extends FOSRestController
 {
@@ -31,10 +36,48 @@ class PetrolStationController extends FOSRestController
      *
      * @return array
      */
-    public function getPetrolStationsAction()
+    public function getPetrolstationsAction()
     {
         $cars = $this->getPetrolStationManager()->findAll();
         return ['petrol_stations' => $cars];
+    }
+
+    /**
+     * Creates a new petrol station from the submitted data.
+     *
+     * @ApiDoc(
+     *   resource = true,
+     *   input = "DataBundle\Form\PetrolStationType",
+     *   statusCodes = {
+     *     200 = "Returned when successful",
+     *     400 = "Returned when the form has errors"
+     *   }
+     * )
+     *
+     * @Annotations\View(
+     * )
+     *
+     * @param Request $request the request object
+     *
+     * @return FormTypeInterface[]|View
+     */
+    public function postPetrolstationsAction(Request $request)
+    {
+        $station = new PetrolStation();
+        $form = $this->createForm(new PetrolStationType(), $station);
+
+        $form->submit($request);
+        if ($form->isValid()) {
+            $em = $this->getEntityManager();
+            $em->persist($station);
+            $em->flush();
+
+            return $this->routeRedirectView('get_petrolstations');
+        }
+
+        return array(
+            'form' => $form
+        );
     }
 
 }
