ed20f443f4180d9bd1543a499390ee40b67bdc10
Fabio Del Bene <delbio87@gmail.com>
2016-04-15 14:35:41 +0200

    9) Come sviluppatore voglio poter effettuare operazioni CRDL sulla entità Car via api Rest

--- src/RestAPIBundle/Controller/CarController.php
+++ src/RestAPIBundle/Controller/CarController.php
@@ -2,11 +2,16 @@
 
 namespace RestAPIBundle\Controller;
 
+use DataBundle\Entity\Car;
+use DataBundle\Form\CarType;
+
 use FOS\RestBundle\Controller\Annotations;
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\View\View;
 
+use Symfony\Component\Form\FormTypeInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class CarController extends FOSRestController
@@ -14,6 +19,11 @@ class CarController extends FOSRestController
     public function getCarManager()
     {
         return $this->getDoctrine()->getRepository('DataBundle:Car');
+    }
+
+    public function getEntityManager()
+    {
+        return $this->getDoctrine()->getManager();
     }
 
     /**
@@ -49,7 +59,7 @@ class CarController extends FOSRestController
      * @Annotations\View(templateVar="car")
      *
      * @param Request $request the request object
-     * @param int     $id      the note id
+     * @param int     $id      the car id
      *
      * @return array
      *
@@ -65,5 +75,94 @@ class CarController extends FOSRestController
         $view = new View($car);
 
         return $view;
+    }
+
+    /**
+     * Creates a new car from the submitted data.
+     *
+     * ApiDoc(
+     *   resource = true,
+     *   input = "DataBundle\Form\CarType",
+     *   statusCodes = {
+     *     200 = "Returned when successful",
+     *     400 = "Returned when the form has errors"
+     *   }
+     * )
+     *
+     * @Annotations\View(
+     * )
+     *
+     * @param Request $request the request object
+     *
+     * @return FormTypeInterface[]|View
+     */
+    public function postCarsAction(Request $request)
+    {
+        $car = new Car();
+        $form = $this->createForm(new CarType(), $car);
+
+        $form->submit($request);
+        if ($form->isValid()) {
+            $em = $this->getEntityManager();
+            $em->persist($car);
+            $em->flush();
+
+            return $this->routeRedirectView('get_car', array('id' => $car->getId()));
+        }
+
+        return array(
+            'form' => $form
+        );
+    }
+
+    /**
+     * Removes a car.
+     *
+     * ApiDoc(
+     *   resource = true,
+     *   statusCodes={
+     *     204="Returned when successful"
+     *   }
+     * )
+     *
+     * @param Request $request the request object
+     * @param int     $id      the car id
+     *
+     * @return View
+     */
+    public function deleteCarsAction(Request $request, $id)
+    {
+        $car = $this->getCarManager()->find($id);
+
+        if (is_null($car)) {
+            throw $this->createNotFoundException("Car does not exist.");
+        }
+        $em = $this->getEntityManager();
+        $em->remove($car);
+        $em->flush();
+
+        // There is a debate if this should be a 404 or a 204
+        // see http://leedavis81.github.io/is-a-http-delete-requests-idempotent/
+        return $this->routeRedirectView('get_cars', array(), Response::HTTP_NO_CONTENT);
+    }
+
+    /**
+     * Removes a note.
+     *
+     * ApiDoc(
+     *   resource = true,
+     *   statusCodes={
+     *     204="Returned when successful"
+     *   }
+     * )
+     *
+     * @param Request $request the request object
+     * @param int     $id      the car id
+     *
+     * @return View
+     */
+    public function removeCarsAction(Request $request, $id)
+    {
+        return $this->deleteCarsAction($request, $id);
     }
 }
